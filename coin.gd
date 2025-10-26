faca5a25b36d70f487cc2e4b722a9ed6f5fa53d6
yuki021007 <87857257+yuki021007@users.noreply.github.com>
2025-10-26 22:21:40 +1100

    Fix scene wiring and UI to improve presentation

--- coin.gd
+++ coin.gd
@@ -1,21 +1,15 @@
 extends Area3D
 
 # Rotation speed
-var rotation_speed = 2.0
+var rotation_speed := 2.0
 
-func _ready():
-	# Connect the body_entered signal
-	body_entered.connect(_on_body_entered)
+func _ready() -> void:
+    body_entered.connect(_on_body_entered)
 
-func _process(delta):
-	# Rotate the coin
-	rotate_y(rotation_speed * delta)
+func _process(delta: float) -> void:
+    rotate_y(rotation_speed * delta)
 
-func _on_body_entered(body):
-	if body.is_in_group("player"):
-		# Tell player to add score
-		body.collect_coin()
-		# Play sound (if you have AudioStreamPlayer)
-		# $AudioStreamPlayer.play()
-		# Remove coin
-		queue_free()
+func _on_body_entered(body: Node) -> void:
+    if body.is_in_group("player"):
+        body.collect_coin()
+        queue_free()
--- goal.gd
+++ goal.gd
@@ -1,31 +1,30 @@
 extends Area3D
 
-func _ready():
-	body_entered.connect(_on_body_entered)
+const REQUIRED_COINS := 5
 
-func _process(delta):
-	# Gentle floating animation
-	position.y += sin(Time.get_ticks_msec() / 500.0) * 0.001
+func _ready() -> void:
+    body_entered.connect(_on_body_entered)
 
-func _on_body_entered(body):
-	if body.is_in_group("player"):
-		# Check if all coins collected
-		if body.score >= 5:
-			show_win_screen()
-		else:
-			show_message("Collect all coins first!")
+func _process(delta: float) -> void:
+    position.y += sin(Time.get_ticks_msec() / 500.0) * 0.001
 
-func show_win_screen():
-	var label = get_node("/root/Main/UI/WinLabel")
-	if label:
-		label.visible = true
-		label.text = "YOU WIN!\nPress R to restart"
+func _on_body_entered(body: Node) -> void:
+    if body.is_in_group("player"):
+        if body.score >= REQUIRED_COINS:
+            show_win_screen()
+        else:
+            show_message("Collect all coins first!")
 
-func show_message(msg):
-	var label = get_node("/root/Main/UI/MessageLabel")
-	if label:
-		label.text = msg
-		label.visible = true
-		# Hide after 2 seconds
-		await get_tree().create_timer(2.0).timeout
-		label.visible = false
+func show_win_screen() -> void:
+    var label := get_node_or_null("/root/Main/UI/WinLabel")
+    if label:
+        label.visible = true
+        label.text = "YOU WIN!\nPress R to restart"
+
+func show_message(msg: String) -> void:
+    var label := get_node_or_null("/root/Main/UI/MessageLabel")
+    if label:
+        label.text = msg
+        label.visible = true
+        await get_tree().create_timer(2.0).timeout
+        label.visible = false
